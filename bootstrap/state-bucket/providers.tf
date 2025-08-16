aad86b4815076b843f99610d9d37490351c8e2b3
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 12:17:14 +0300

    update

--- bootstrap/state-bucket/providers.tf
+++ bootstrap/state-bucket/providers.tf
@@ -1,8 +1,13 @@
+# bootstrap/state-bucket/providers.tf
 provider "oci" {
-  region            = var.region
-  tenancy_ocid      = var.tenancy_ocid
-  user_ocid         = var.user_ocid
-  fingerprint       = var.fingerprint
-  private_key_path  = var.private_key_path
-  # (alternatively you could use env-only by omitting fields, since OCI_* are set)
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
+# bootstrap/state-bucket/variables.tf
+variable "private_key_path" {
+  description = "Path to the PEM private key on the runner/host"
+  type        = string
 }
