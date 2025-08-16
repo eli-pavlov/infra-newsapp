849a0f89123451e51da879cfa0fabb3c15e5a708
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 21:07:03 +0300

    update

---
+++ modules/cluster/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 6.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
