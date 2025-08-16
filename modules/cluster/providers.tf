601160672dfeca3fc7728709be4ccd411bf0a40b
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 16:26:28 +0300

    update

--- modules/cluster/providers.tf
+++ modules/cluster/providers.tf
@@ -20,22 +20,62 @@ terraform {
   }
 }
 
+# --- Variable Declarations ---
+
+# The tenancy_ocid is a required argument that uniquely identifies
+# your tenancy in OCI.
+variable "tenancy_ocid" {
+  type        = string
+  description = "The OCID of the tenancy."
+}
+
+# The user_ocid is a required argument that identifies the user account
+# that Terraform will use for authentication.
+variable "user_ocid" {
+  type        = string
+  description = "The OCID of the user."
+}
+
+# The fingerprint corresponds to the fingerprint of the public key
+# associated with the user.
+variable "fingerprint" {
+  type        = string
+  description = "The fingerprint of the user's public key."
+}
+
+# The private_key_path tells the OCI provider where to find the private key
+# file on the runner. This is the key that was written from the secret.
+variable "private_key_path" {
+  type        = string
+  description = "The path to the OCI private key file on the runner."
+}
+
+# The region is the geographical location where resources will be created.
+variable "region" {
+  type        = string
+  description = "The OCI region to deploy resources into."
+}
+
+# --- Provider Configuration ---
+
 # Configure the OCI provider. The values for these arguments are
 # automatically passed in from the GitHub Actions workflow environment
 # variables, which are prefixed with `TF_VAR_`.
 provider "oci" {
   # The tenancy_ocid is a required argument that uniquely identifies
-  # your tenancy in OCI.
-  tenancy_ocid = var.tenancy_ocid
+  # your tenancy in OCI. It's set from the `tenancy_ocid` variable.
+  tenancy_ocid     = var.tenancy_ocid
   # The user_ocid is a required argument that identifies the user account
-  # that Terraform will use for authentication.
-  user_ocid = var.user_ocid
+  # that Terraform will use for authentication. It's set from the `user_ocid` variable.
+  user_ocid        = var.user_ocid
   # The fingerprint corresponds to the fingerprint of the public key
-  # associated with the user.
-  fingerprint = var.fingerprint
+  # associated with the user. It's set from the `fingerprint` variable.
+  fingerprint      = var.fingerprint
   # The private_key_path tells the OCI provider where to find the private key
   # file on the runner. This is the key that was written from the secret.
+  # It's set from the `private_key_path` variable.
   private_key_path = var.private_key_path
   # The region is the geographical location where resources will be created.
-  region = var.region
+  # It's set from the `region` variable.
+  region           = var.region
 }
