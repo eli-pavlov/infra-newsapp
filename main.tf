2a0f7b65ec9d47427b9950569afa9a2aa4aed009
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 16:38:28 +0300

    update

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
-  region           = var.region
-}
 
 module "network" {
   source            = "./modules/network"
--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
 provider "oci" {
   auth = "APIKey" # picks up OCI_* env vars
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
 }
