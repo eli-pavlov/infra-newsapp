498472a6bd19c5659a61649aa1cb4f8a5c5d41d4
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 16:38:39 +0300

    updaet

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "oci" {
-  auth = "APIKey" # picks up OCI_* env vars
+  auth             = "APIKey" # picks up OCI_* env vars
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
