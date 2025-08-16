b62f56ffc0b968809a1140d44f6f59c01c76e7c9
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 11:33:49 +0300

    update

--- provider.tf
+++ provider.tf
@@ -1,29 +1,3 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "~> 6.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.3"
-    }
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.4"
-    }
-  }
-}
-
 provider "oci" {
-  region        = var.region
-  tenancy_ocid  = var.tenancy_ocid
-  user_ocid     = var.user_ocid
-  fingerprint   = var.fingerprint
-  private_key_path = var.private_key_path
+  auth = "APIKey" # picks up OCI_* env vars
 }
