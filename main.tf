702a7e5042287c6373af0108b94eb30a6cbf5922
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 18:07:03 +0300

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 5.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
 module "network" {
   source            = "./modules/network"
   region            = var.region
