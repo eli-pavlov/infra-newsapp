4d7922c2d776ff743c61ddfd88dfec3f3b9b5d85
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 17:47:06 +0300

    update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "oci" {
   user_ocid    = var.user_ocid
   fingerprint  = var.fingerprint
   region       = var.region
-  private_key = var.private_key_pem
+  private_key  = var.private_key_pem
 }
 
 module "network" {
