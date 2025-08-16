a3e7f22c1f062031e2110707de5bd748e9e144ca
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 15:11:12 +0300

    update

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -30,13 +30,7 @@ output "private_lb_id" {
   description = "The OCID of the private load balancer."
   value       = oci_load_balancer_load_balancer.k3s_private_lb.id
 }
-
-output "private_lb_ip_address" {
-  description = "The private IP address of the load balancer."
-  value       = oci_load_balancer_load_balancer.k3s_private_lb.ip_address_details[0].ip_address
-}
-
-output "private_lb_security_group" {
-  description = "The OCID of the security group for the private load balancer."
-  value       = oci_core_network_security_group.private_lb.id
+output "public_nlb_id" {
+  description = "The OCID of the public network load balancer."
+  value       = oci_network_load_balancer_network_load_balancer.k3s_public_lb.id
 }
