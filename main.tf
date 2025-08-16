4c54d9190ae63c1d0adf6bb3a3ee56b370c913e9
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 17:46:59 +0300

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "oci" {
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  fingerprint  = var.fingerprint
+  region       = var.region
+  private_key = var.private_key_pem
+}
 
 module "network" {
   source            = "./modules/network"
--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.fingerprint
-  region       = var.region
-
-  # This line uses the private key content directly, as configured in the last steps.
-  private_key = var.private_key_pem
-}
