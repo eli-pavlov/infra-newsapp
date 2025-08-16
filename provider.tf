eb5819bc95988a0266219df4b947c04e554e9c6d
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 17:29:14 +0300

    udpate

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "oci" {
   user_ocid    = var.user_ocid
   fingerprint  = var.fingerprint
   region       = var.region
-  
+
   # This line uses the private key content directly, as configured in the last steps.
-  private_key  = var.private_key_pem 
+  private_key = var.private_key_pem
 }
