0fd207f980b468f92b7dd6e3136830db6c7604f3
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 17:13:58 +0300

    update

--- modules/cluster/providers.tf
+++ modules/cluster/providers.tf
@@ -43,9 +43,9 @@ variable "private_key_path" {
 }
 
 provider "oci" {
-  tenancy_ocid    = var.tenancy_ocid
-  user_ocid       = var.user_ocid
-  fingerprint     = var.fingerprint
-  private_key     = var.private_key_pem
-  region          = var.region
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid    = var.user_ocid
+  fingerprint  = var.fingerprint
+  private_key  = var.private_key_pem
+  region       = var.region
 }
