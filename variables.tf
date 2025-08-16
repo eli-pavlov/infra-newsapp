b8b72084d8cfc28d07a47b047739c9ad0b00d33b
eli-pavlov <pavlov.eli@gmail.com>
2025-08-16 12:22:14 +0300

    update

--- variables.tf
+++ variables.tf
@@ -1,16 +1,29 @@
-variable "tenancy_ocid"       { type = string, sensitive = true }
-variable "user_ocid"          { type = string, sensitive = true }
-variable "fingerprint"        { type = string, sensitive = true }
-variable "private_key_path"   { type = string, sensitive = true }
-variable "region"             { type = string }
+variable "tenancy_ocid" {
+  type      = string
+  sensitive = true
+}
 
-# From your env.auto.tfvars that triggered warnings:
-variable "availability_domain" { type = string, default = null }
-variable "admin_cidr"          { type = string, default = null }
-variable "os_image_id"         { type = string, default = null }
-variable "cluster_name"        { type = string, default = null }
-variable "bucket_name"         { type = string, default = null }
-variable "os_namespace"        { type = string, default = null }
+variable "admin_cidr" {
+  type    = string
+  default = null
+}
 
-# Anything your modules require will already be declared inside modules;
-# add more top-level variables here only if Terraform complains again.
+variable "os_image_id" {
+  type    = string
+  default = null
+}
+
+variable "cluster_name" {
+  type    = string
+  default = null
+}
+
+variable "bucket_name" {
+  type    = string
+  default = null
+}
+
+variable "os_namespace" {
+  type    = string
+  default = null
+}
