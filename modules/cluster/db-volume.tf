f6b4505c10d49a8dc5907028c23dbb2b7a84ff1e
eli-pavlov <pavlov.eli@gmail.com>
2025-08-18 16:40:56 +0300

    update

--- modules/cluster/db-volume.tf
+++ modules/cluster/db-volume.tf
@@ -5,10 +5,15 @@ resource "oci_core_volume" "db" {
   display_name        = "k3s-db-volume"
 }
 
+# Attach to the first server instance from the k3s_servers pool
 resource "oci_core_volume_attachment" "db_to_first_server" {
   compartment_id  = var.compartment_ocid
-  instance_id     = var.first_server_instance_id
+  instance_id     = data.oci_core_instance_pool_instances.k3s_servers_instances.instances[0].id
   volume_id       = oci_core_volume.db.id
   attachment_type = "paravirtualized"
   device          = var.db_volume_device
+
+  depends_on = [
+    data.oci_core_instance_pool_instances.k3s_servers_instances
+  ]
 }
