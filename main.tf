e81980fe90da755312735507f5a817e9133220c0
Eli Pavlov <pavlov.eli@gmail.com>
2025-09-10 18:59:43 +0300

    update

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ module "cluster" {
   public_nlb_backend_set_http_name  = module.network.public_nlb_backend_set_http_name
   public_nlb_backend_set_https_name = module.network.public_nlb_backend_set_https_name
   private_lb_backendset_name        = module.network.private_lb_backendset_name
+  private_subnet_cidr   = module.network.private_subnet_cidr
 }
 
 # Optional: write a tiny JSON with a couple of useful outputs into your bucket
