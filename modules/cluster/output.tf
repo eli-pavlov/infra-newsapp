1bdf140b36b0bdeb5289ac6f4f72783b292f4ebc
Eli Pavlov <pavlov.eli@gmail.com>
2025-09-11 16:36:52 +0300

    update

--- modules/cluster/output.tf
+++ modules/cluster/output.tf
@@ -1,67 +1,34 @@
-# K3s token for cluster join authentication
-resource "random_password" "k3s_token" {
-  length  = 55
-  special = false
+# modules/cluster/output.tf
+
+output "bastion_public_ip" {
+  description = "Public IP of the bastion host for SSH access."
+  # Now outputs the value of the reserved public IP resource.
+  value       = oci_core_public_ip.bastion.ip_address
 }
 
-# =================== CONTROL-PLANE cloud-init ===================
-data "cloudinit_config" "k3s_server_tpl" {
-  gzip            = true
-  base64_encode   = true
-
-  part {
-    content_type = "text/x-shellscript"
-    content = templatefile("${path.module}/files/k3s-install-server.sh", {
-      T_K3S_VERSION          = var.k3s_version,
-      T_K3S_TOKEN            = random_password.k3s_token.result,
-      T_DB_USER              = var.db_user,
-      T_DB_NAME_DEV          = var.db_name_dev,
-      T_DB_NAME_PROD         = var.db_name_prod,
-      T_DB_SERVICE_NAME_DEV  = var.db_service_name_dev,
-      T_DB_SERVICE_NAME_PROD = var.db_service_name_prod,
-      T_MANIFESTS_REPO_URL   = var.manifests_repo_url,
-      T_EXPECTED_NODE_COUNT  = local.expected_total_node_count,
-      T_PRIVATE_LB_IP        = var.private_lb_ip_address
-    })
-  }
+output "control_plane_private_ip" {
+  description = "Private IP of the control-plane node."
+  value       = data.oci_core_vnic.cp.private_ip_address
 }
 
-# =================== VNIC lookups for instance IPs ===================
-
-# Control plane
-data "oci_core_vnic_attachments" "cp" {
-  compartment_id = var.compartment_ocid
-  instance_id    = oci_core_instance.control_plane.id
-}
-data "oci_core_vnic" "cp" {
-  vnic_id = data.oci_core_vnic_attachments.cp.vnic_attachments[0].vnic_id
+output "app_worker_private_ips" {
+  description = "Private IPs of the application worker nodes."
+  value       = [for _, v in data.oci_core_vnic.app : v.private_ip_address]
 }
 
-# Bastion
-data "oci_core_vnic_attachments" "bastion" {
-  compartment_id = var.compartment_ocid
-  instance_id    = oci_core_instance.bastion.id
-}
-data "oci_core_vnic" "bastion" {
-  vnic_id = data.oci_core_vnic_attachments.bastion.vnic_attachments[0].vnic_id
+output "db_worker_private_ip" {
+  description = "Private IP of the database worker node."
+  value       = data.oci_core_vnic.db.private_ip_address
 }
 
-# App workers (map by index)
-data "oci_core_vnic_attachments" "app" {
-  for_each       = { for idx, inst in oci_core_instance.app_workers : idx => inst.id }
-  compartment_id = var.compartment_ocid
-  instance_id    = each.value
-}
-data "oci_core_vnic" "app" {
-  for_each = data.oci_core_vnic_attachments.app
-  vnic_id  = each.value.vnic_attachments[0].vnic_id
+output "public_nlb_backend_set_http_name" {
+  value = module.network.public_nlb_backend_set_http_name
 }
 
-# DB worker
-data "oci_core_vnic_attachments" "db" {
-  compartment_id = var.compartment_ocid
-  instance_id    = oci_core_instance.db_worker.id
+output "public_nlb_backend_set_https_name" {
+  value = module.network.public_nlb_backend_set_https_name
 }
-data "oci_core_vnic" "db" {
-  vnic_id = data.oci_core_vnic_attachments.db.vnic_attachments[0].vnic_id
+
+output "private_lb_backendset_name" {
+  value = module.network.private_lb_backendset_name
 }
