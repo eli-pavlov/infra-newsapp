a742e1106d5f9cbc8efc79161503278951e32943
eli-pavlov <pavlov.eli@gmail.com>
2025-09-15 22:37:04 +0300

    update

--- main.tf
+++ main.tf
@@ -38,9 +38,9 @@ module "cluster" {
   public_nlb_backend_set_https_name = module.network.public_nlb_backend_set_https_name
   private_lb_backendset_name        = module.network.private_lb_backendset_name
   
-  # depends_on = [
-  # module.network
-  # ]
+  depends_on = [
+  module.network
+  ]
 }
 
 # Optional: write a tiny JSON with a couple of useful outputs into your bucket
