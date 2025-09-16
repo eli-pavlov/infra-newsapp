83cca73d588661991468442ce928f093f9263310
Eli Pavlov <pavlov.eli@gmail.com>
2025-09-16 09:23:42 +0300

    update

--- modules/network/dns.tf
+++ modules/network/dns.tf
@@ -9,7 +9,7 @@ variable "cloudflare_zone_id" {
 resource "cloudflare_dns_record" "argocd" {
   zone_id = var.cloudflare_zone_id
   name    = "argocd"
-  content   = module.network.public_nlb_ip_address
+  content = oci_network_load_balancer_network_load_balancer.public_nlb.ip_addresses[0].ip_address
   type    = "A"
   ttl     = 1 # Automatic
   proxied = true
@@ -50,7 +50,7 @@ resource "cloudflare_ruleset" "argocd_admin_only" {
 resource "cloudflare_dns_record" "newsapp_dev" {
   zone_id = var.cloudflare_zone_id
   name    = "newsapp-dev"
-  content   = module.network.public_nlb_ip_address
+  content = oci_network_load_balancer_network_load_balancer.public_nlb.ip_addresses[0].ip_address
   type    = "A"
   ttl     = 1 # Automatic
   proxied = true
@@ -60,7 +60,7 @@ resource "cloudflare_dns_record" "newsapp_dev" {
 resource "cloudflare_dns_record" "newsapp_prod" {
   zone_id = var.cloudflare_zone_id
   name    = "newsapp"
-  content   = module.network.public_nlb_ip_address
+  content = oci_network_load_balancer_network_load_balancer.public_nlb.ip_addresses[0].ip_address
   type    = "A"
   ttl     = 1 # Automatic
   proxied = true
