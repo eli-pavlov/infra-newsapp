9dd541500465b555bd4ce0f42299e17819a31110
Eli Pavlov <pavlov.eli@gmail.com>
2025-09-24 14:10:17 +0300

    update

--- terraform/1-storage/providers.tf
+++ terraform/1-storage/providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 7.18.0" # Match the version used elsewhere for consistency
+    }
+  }
+}
+
 provider "oci" {
   tenancy_ocid = var.oci_tenancy_ocid
   user_ocid    = var.oci_user_ocid
