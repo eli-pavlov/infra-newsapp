3cbdca56bb358631039a65eb65997680cbf93f42
Eli Pavlov <pavlov.eli@gmail.com>
2025-09-24 14:06:02 +0300

    update

--- terraform/1-storage/providers.tf
+++ terraform/1-storage/providers.tf
@@ -1,2 +1,7 @@
 provider "oci" {
+  tenancy_ocid = var.oci_tenancy_ocid
+  user_ocid    = var.oci_user_ocid
+  fingerprint  = var.oci_fingerprint
+  region       = var.oci_region
+  private_key  = var.oci_private_key_pem
 }
